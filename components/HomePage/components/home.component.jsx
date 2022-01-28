de6de41db6d589275d3ac5161b6e2b397c48a3ad
Maxi <claudioacevedo25@gmail.com>
2022-01-28 01:02:52 -0300

    delete some text hardcode

--- components/HomePage/components/home.component.jsx
+++ components/HomePage/components/home.component.jsx
@@ -4,13 +4,15 @@ const HomePage = () => {
     <div className={style.container}>
       <div className={style.videoContainer}>
         <video className={style.video} autoPlay loop muted>
-            <source src="video/psicoterapia1.mp4" type="video/mp4" />
-          </video>
+          <source src="video/psicoterapia1.mp4" type="video/mp4" />
+        </video>
       </div>
       <div className={style.content}>
         <p>
+          {`
           Where does it come from?
           Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem  comes from a line in section 1.10.32.
+        `}
         </p>
       </div>
     </div>
