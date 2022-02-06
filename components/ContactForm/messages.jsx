e1b5714540d61d1eb30678eabd2d4cb9f08081d2
Maxi <claudioacevedo25@gmail.com>
2022-02-06 00:50:12 -0300

    feat add snackbar messages

---
+++ components/ContactForm/messages.jsx
@@ -0,0 +1,10 @@
+export const messages = {
+    success: {
+        id: 1,
+        defaultMessage: 'Mensaje enviado, te voy a estar respondiendo lo antes posible.'
+    },
+    error: {
+        id:2,
+        defaultMessage: 'Ocurrió un error al intentar mandar tu consulta. Intenta de nuevo más tarde'
+    }
+}
--- components/ContactPage/components/contact.component.jsx
+++ components/ContactPage/components/contact.component.jsx
@@ -1,17 +1,17 @@
-import ContactForm from "../../ContactForm";
-import style from '../contact.module.css';
-import SocialMedia from '../../footer.component/socialMedia';
 import { Typography } from "@mui/material";
+import ContactForm from "../../ContactForm";
+import SocialMedia from '../../footer.component/socialMedia';
+import style from '../contact.module.css';
 const ContactPage = () => {
   return (
     <div
       className={style.container}
     >
-      <div className={style.socialMedia}>
+      <div  className={style.socialMedia}>
         <Typography align="center" component='h1' variant='h5'>Seguime en las redes</Typography>
         <SocialMedia />
       </div>
-      <Typography  gutterBottom mt={2} component='h2' variant='h4'>Contactame</Typography>
+      <Typography gutterBottom mt={2} component='h2' variant='h4'>Contactame</Typography>
       <ContactForm />
     </div>
   )
