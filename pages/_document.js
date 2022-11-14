bcea080849f2060b030f20ebfa3849f11bb043dd
Maxi <mpezzotta@folcode.com>
2022-11-13 23:23:28 -0300

    rename var

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='es'>
+    <Html lang="es">
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="description" content="Licenciada Romina Gambino" />
         <link rel="icon" href="img/cerebro.png" />
       </Head>
@@ -13,5 +13,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
